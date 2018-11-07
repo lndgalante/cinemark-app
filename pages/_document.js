4044a31d0cee9332c363c05e50b4d3531e9462a3
Juan Salvatore <juansalvatore@live.com.ar>
2018-11-07 16:54:43 -0300

    Add server side rendering to evegreen-ui library

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { extractStyles } from 'evergreen-ui'
 
 class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
@@ -11,12 +12,29 @@ class MyDocument extends Document {
     return { ...page, styleTags }
   }
 
+  static getInitialProps({ renderPage }) {
+    const page = renderPage()
+    const { css, hydrationScript } = extractStyles()
+
+    return {
+      ...page,
+      css,
+      hydrationScript,
+    }
+  }
+
   render() {
+    const { css, hydrationScript } = this.props
+
     return (
       <html lang="es">
-        <Head>{this.props.styleTags}</Head>
+        <Head>
+          {this.props.styleTags}
+          <style dangerouslySetInnerHTML={{ __html: css }} />
+        </Head>
         <body>
           <Main />
+          {hydrationScript}
           <NextScript />
         </body>
       </html>
