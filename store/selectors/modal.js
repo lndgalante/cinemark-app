51e211bb48ce679a23576331d3769b515ccc5795
lndgalante <lndgalante@gmail.com>
2018-11-07 04:14:00 -0300

    Fix modal selector

--- store/selectors/modal.js
+++ store/selectors/modal.js
@@ -5,10 +5,8 @@ const getPremiereCinemas = state => state.movies.selectedPremiere.cinemas
 const getSelectedCinema = state => state.select.selectedCinema.value
 
 const modalSelector = createSelector([getPremiereCinemas, getSelectedCinema], (movieCinemas, selectedCinema) => {
-  if (!selectedCinema) {
-    // toaster.warning('Primero debes elegir tu cine preferido')
-    return null
-  }
+  if (!selectedCinema) return null
+  if (!movieCinemas) return null
 
   return movieCinemas.hasOwnProperty(selectedCinema) ? movieCinemas[selectedCinema] : null
 })
