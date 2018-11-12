d4e6aa727f69a32791caadf591a1a4e3077c5907
lndgalante <lndgalante@gmail.com>
2018-11-12 10:44:48 -0300

    Clean dependencies

--- components/shared/Trailer.js
+++ components/shared/Trailer.js
@@ -1,15 +1,14 @@
 import { Pane } from 'evergreen-ui'
-import YouTube from 'react-youtube'
+import styled from 'styled-components'
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+`
 
 const Trailer = ({ videoId }) => (
   <Pane height={297}>
-    <YouTube
-      videoId={videoId}
-      opts={{
-        width: '100%',
-        height: '100%',
-      }}
-    />
+    <Iframe src={`https://www.youtube.com/embed/${videoId}`} />
   </Pane>
 )
 
