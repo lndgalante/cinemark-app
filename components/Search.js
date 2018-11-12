cb474b1fa5263edd1f7fb59ac15aea3c8850d5c5
lndgalante <lndgalante@gmail.com>
2018-11-12 11:30:52 -0300

    Add Label

--- components/Search.js
+++ components/Search.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { SearchInput } from 'evergreen-ui'
 import debounce from 'lodash.debounce'
+import styled from 'styled-components'
+
+const Label = styled.label`
+  display: none;
+`
 
 class Search extends Component {
   constructor(props) {
@@ -30,13 +35,17 @@ class Search extends Component {
     const { query } = this.state
 
     return (
-      <SearchInput
-        height={40}
-        value={query}
-        onChange={this.onChange}
-        className="search-input"
-        placeholder="Buscar película"
-      />
+      <Fragment>
+        <Label htmlFor="search-input">Buscar película</Label>
+        <SearchInput
+          height={40}
+          value={query}
+          onChange={this.onChange}
+          id="search-input"
+          className="search-input"
+          placeholder="Buscar película"
+        />
+      </Fragment>
     )
   }
 }
