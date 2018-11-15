df25edb95ccf213eb681242a0672e648003a4183
lndgalante <lndgalante@gmail.com>
2018-11-15 19:46:50 -0300

    Add manifest. Bump dependencies

--- components/Meta.js
+++ components/Meta.js
@@ -4,10 +4,11 @@ const Meta = () => (
   <Head>
     <title>Cinemark Estrenos</title>
 
+    <link rel="manifest" href="./static/manifest.json" />
+
     <link rel="apple-touch-icon" sizes="180x180" href="./static/favicons/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="./static/favicons/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="./static/favicons/favicon-16x16.png" />
-    <link rel="manifest" href="./static/favicons/site.webmanifest" />
     <link rel="mask-icon" href="./static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
     <link rel="shortcut icon" href="./static/favicons/favicon.ico" />
 
